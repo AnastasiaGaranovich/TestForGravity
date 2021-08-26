d241ce22af9aa83666a77cccc37bd5be743c1dcd
AnastasiaGaranovich <nastyamadness@gmail.com>
2021-08-26 12:08:31 +0300

    Create project, add .gitignore

---
+++ TestForGravity/TestForGravity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestForGravity
+//
+//  Created by Анастасия Гаранович on 26.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
